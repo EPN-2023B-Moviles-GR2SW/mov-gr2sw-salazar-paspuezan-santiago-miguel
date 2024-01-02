311df69868b802af4d87857c72950902f06ec3c3
SantiagoMSalazar <santiago.salazar@epn.edu.ec>
2024-01-02 09:11:27 -0500

    Creación de sentencias con SQLite
    clase 15  02/enero/2023

---
+++ 02-Android/app/src/main/java/com/example/b2023sw/ESqliteHelperEntrenador.kt
@@ -0,0 +1,80 @@
+package com.example.b2023sw
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ESqliteHelperEntrenador(
+    contexto: Context?,
+):SQLiteOpenHelper(
+    contexto,
+    "moviles",
+    null,
+    1
+    ){
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val scriptSQLCrearTablaEntrenador =
+            """
+                CREATE TABLE ENTRENADOR(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre VARCHAR(50),
+                descripcion VARCHAR(50)
+                )
+            """.trimIndent()
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+    fun crearEntrenador(
+        nombre: String,
+        description:String
+    ):Boolean{
+        val basedatosEscritura = writableDatabase
+        val valoresAGuardar = ContentValues()
+        valoresAGuardar.put("nombre", nombre)
+        valoresAGuardar.put("descripcion", description)
+        val resultadoGuardar: Long = basedatosEscritura
+            .insert(
+                "ENTRENADOR",
+                null,
+                valoresAGuardar
+            )
+        basedatosEscritura.close()
+        return if (resultadoGuardar.toInt() == -1) false else true
+    }
+    fun eliminarEntradorFormulario(id: Int):Boolean{
+        val conexionEscritura = writableDatabase
+        // Qué id hay que borrar?
+        val parametrosConsultaDelete = arrayOf(id.toString())
+        val resultadoEliminacion = conexionEscritura
+            .delete(
+                "ENTRENADOR",
+                "id=?",
+                parametrosConsultaDelete
+            )
+        conexionEscritura.close()
+        return if (resultadoEliminacion.toInt() == -1) false else true
+    }
+    fun actualizarEntrenadorFormulario(
+        nombre: String,
+        descripcion: String,
+        id: Int
+    ):Boolean{
+        val conexionEscritura = writableDatabase
+        val valoresActualizar = ContentValues()
+        valoresActualizar.put("nombre", nombre)
+        valoresActualizar.put("descripcion", descripcion)
+        // Qué id hay que actualizar
+        val parametrosConsultaActualizar = arrayOf(id.toString())
+        val resultadoActualizacion = conexionEscritura
+            .update(
+                "ENTRENADOR",
+                valoresActualizar,
+                "id=?",
+                parametrosConsultaActualizar
+            )
+        conexionEscritura.close()
+        return if (resultadoActualizacion.toInt() == -1) false else true
+    }
+}
